8ac1f1d40101ebf64c23bf58e4c8e04d9e2d4d07
tuttifrutti <81527089+tuttifrutti@users.noreply.github.com>
2021-03-28 22:17:33 +0500

    Add files via upload

---
+++ practice_question1.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class practice_question1 {
+    public static void main(String args[]){
+        for(int i=10; i>1; i--){
+            System.out.println("The value of i is: "+i);
+        }
+    }
+}
